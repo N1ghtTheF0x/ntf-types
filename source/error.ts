821556dcc6404041bda724bbb1dbcd7fead30449
N1ghtTheF0x <kilo.night.prower@gmail.com>
2026-01-05 23:06:33 +0100

    impossible error

--- source/error.ts
+++ source/error.ts
@@ -4,7 +4,6 @@
 export class ExpectedTypeError extends TypeError
 {
     /**
-     * 
      * @param expected The type that was expected
      * @param received The value that was recevied instead
      * @param options Additional error options
@@ -12,5 +11,19 @@ export class ExpectedTypeError extends TypeError
     public constructor(public readonly expected: string,public readonly received: unknown,options?: ErrorOptions)
     {
         super(`expected '${expected}', received '${received}'`,options)
+    }
+}
+
+/**
+ * A error class that is impossible to reach. The JavaScript runtime would be **REALLY** problematic if you receive this error
+ */
+export class ImpossibleError extends TypeError
+{
+    /**
+     * @param options Additional error options
+     */
+    public constructor(options?: ErrorOptions)
+    {
+        super("How did you got here?",options)
     }
 }
