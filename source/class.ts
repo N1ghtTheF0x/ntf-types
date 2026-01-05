8cb7355ee1a927debbdba4a1770cfa3dc9f411bf
N1ghtTheF0x <kilo.night.prower@gmail.com>
2026-01-05 23:06:24 +0100

    get contructor

--- source/class.ts
+++ source/class.ts
@@ -1,3 +1,6 @@
+import { isNotNullLike, NullLike } from "./null"
+import { isAnyTypeof, Typeof } from "./typeof"
+
 /**
  * A typed function constructor
  * @template Args The arguments that are required for the constructor
@@ -20,4 +23,39 @@ export function ImplementsStatic<T>(): <Class extends T & AbstractClassConstruct
     {
         return Class
     }
+}
+
+/**
+ * A mapped type that contains some primitive constructor types
+ */
+export type PrimitiveConstructors = {
+    "string": StringConstructor
+    "boolean": BooleanConstructor
+    "number": NumberConstructor
+    "bigint": BigIntConstructor
+    "symbol": SymbolConstructor
+}
+
+/**
+ * Get the constructor function from `value`. Will return `undefined` if `value` is `null` or `undefined`
+ * @param value A value
+ */
+export function getConstructor<Args extends Array<any>,T>(value: T): ClassConstructor<Args,T>
+export function getConstructor(value: NullLike): undefined
+export function getConstructor<T extends keyof PrimitiveConstructors>(value: Typeof[T]): PrimitiveConstructors[T]
+export function getConstructor<Args extends Array<any>,T>(value: T | NullLike): ClassConstructor<Args,T> | PrimitiveConstructors[keyof PrimitiveConstructors] | undefined
+{
+    if(typeof value == "bigint")
+        return BigInt
+    if(typeof value == "boolean")
+        return Boolean
+    if(typeof value == "number")
+        return Number
+    if(typeof value == "string")
+        return String
+    if(typeof value == "symbol")
+        return Symbol
+    if(isNotNullLike(value) && isAnyTypeof(value,"object","function") && "constructor" in value)
+        return value.constructor as ClassConstructor<Args,T>
+    return undefined
 }
